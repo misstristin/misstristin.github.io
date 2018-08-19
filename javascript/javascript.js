c9c8d3d9b96848c4aa832cce17cb73102da48286
Tristin Tobias <tristintobias@gmail.com>
2018-08-18 17:03:42 -0700

    made changes to syling and added links via jquery

---
+++ javascript/javascript.js
@@ -0,0 +1,59 @@
+// List of projects with data
+
+var projectList = [
+    {
+        name: 'Basic Wireframe',
+        url: 'http://misstristin.github.io/HW-Wireframe',
+        thumb: ''
+    },
+    {
+        name: 'Basic Portfolio',
+        url: 'http://misstristin.github.io/Basic-Portfolio',
+        thumb: ''
+    },
+    {
+        name: 'Responsive Portfolio',
+        url: 'http://misstristin.github.io/Responsive-Portfolio',
+        thumb: ''
+    },
+    {
+        name: 'Psychic Game',
+        url: 'http://misstristin.github.io/psychic-game',
+        thumb: ''
+    },
+    {
+        name: 'Harry Potter Hangman',
+        url: 'http://misstristin.github.io/word-guess-game',
+        thumb: ''
+    },
+    {
+        name: 'Crystal BlackJack',
+        url: 'http://misstristin.github.io/unit-4-game',
+        thumb: ''  
+    },
+    {
+        name: 'Supernatural TV Show Trivia',
+        url: 'http://misstristin.github.io/TriviaGame',
+        thumb: ''  
+    },
+    {
+        name: 'Monopoly Project Collab',
+        url: 'http://tayloraanenson.github.io/monopoly-project',
+        thumb: ''  
+    }
+];
+
+// Adding project links to page
+
+//variables
+var i, li, a;
+
+for (i=0; i<projectList.length; i++){
+    li = $('<li>');   
+    a = $('<a>');
+    a.attr('href', projectList[i].url);
+    a.attr('target', '_blank');
+    a.text(projectList[i].name);
+    li.append(a);
+    $('#linkList').append(li);  
+}
